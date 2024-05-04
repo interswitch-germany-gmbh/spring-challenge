d5198db686f32f2dc754528cdaaaf8da91ffbbc6
Bashar deeb <bashardeeb77@gmail.com>
2024-05-04 15:11:01 +0200

    add spring-cloud-starter-openfeign Dependency to pom

--- src/main/java/com/isw/assessment/ChallengeApplication.java
+++ src/main/java/com/isw/assessment/ChallengeApplication.java
@@ -2,9 +2,11 @@ package com.isw.assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- 
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class ChallengeApplication
 {
   public static void main(String[] args)
