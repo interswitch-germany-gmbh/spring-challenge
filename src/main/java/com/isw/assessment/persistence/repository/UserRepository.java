74a8631877a4e96e8567fd4c617bf855e23b691e
Bashar deeb <bashardeeb77@gmail.com>
2024-05-04 15:47:58 +0200

    add findFirstByEmail(String email) to UserRepository and relocate UserRepository

--- src/main/java/com/isw/assessment/persistence/repository/UserRepository.java
+++ src/main/java/com/isw/assessment/persistence/repository/UserRepository.java
@@ -1,13 +1,15 @@
-package com.isw.assessment.repository;
- 
+package com.isw.assessment.persistence.repository;
 
+
+import com.isw.assessment.persistence.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.isw.assessment.entity.UserEntity;
+import java.util.Optional;
 
- 
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findFirstByEmail(String email);
+
 }
