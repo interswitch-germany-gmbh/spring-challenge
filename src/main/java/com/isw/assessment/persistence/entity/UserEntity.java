37c5a4e6f27cb37ae1edb0ae24be5121f5241a56
Bashar deeb <bashardeeb77@gmail.com>
2024-05-04 15:50:31 +0200

    relocate UserEntity

---
+++ src/main/java/com/isw/assessment/persistence/entity/UserEntity.java
@@ -0,0 +1,71 @@
+package com.isw.assessment.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(indexes = @Index(columnList = "email"))
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+                + ", phone=" + phone + "]";
+    }
+
+
+}
