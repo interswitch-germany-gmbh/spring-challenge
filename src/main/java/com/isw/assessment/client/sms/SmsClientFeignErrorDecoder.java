4a5f117e202975c2e4b7f5ebad94fe7a970ba796
Bashar deeb <bashardeeb77@gmail.com>
2024-05-04 18:01:35 +0200

    SmsClientFeignErrorDecoder refactored

--- src/main/java/com/isw/assessment/client/sms/SmsClientFeignErrorDecoder.java
+++ src/main/java/com/isw/assessment/client/sms/SmsClientFeignErrorDecoder.java
@@ -2,7 +2,9 @@ package com.isw.assessment.client.sms;
 
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import org.apache.tomcat.util.codec.binary.StringUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.BufferedReader;
@@ -13,18 +15,10 @@ public class SmsClientFeignErrorDecoder implements ErrorDecoder {
 
     @Override
     public Exception decode(String methodKey, Response response) {
-
         String responseBody = readResponseBody(response);
-        return switch (response.status()) {
-            case 400 -> new ResponseStatusException(HttpStatus.BAD_REQUEST, responseBody);
-            case 401 -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, responseBody);
-            case 403 -> new ResponseStatusException(HttpStatus.FORBIDDEN);
-            case 404 -> new ResponseStatusException(HttpStatus.NOT_FOUND);
-            case 408 -> new ResponseStatusException(HttpStatus.REQUEST_TIMEOUT, responseBody);
-            case 500 -> new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, responseBody);
-            default -> new RuntimeException(responseBody);
-        };
+        responseBody= responseBody.isBlank() ? null : responseBody;
 
+        return new ResponseStatusException(HttpStatus.valueOf(response.status()), responseBody);
     }
 
     private static String readResponseBody(Response response) {
@@ -32,7 +26,7 @@ public class SmsClientFeignErrorDecoder implements ErrorDecoder {
         try (BufferedReader bufferedReader = new BufferedReader(response.body().asReader(StandardCharsets.UTF_8))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line).append("\n");
+                stringBuilder.append(line);
             }
         } catch (IOException e) {
             return "";
