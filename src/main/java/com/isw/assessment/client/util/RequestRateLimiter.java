aea1a541905b136d4bfa978650e3400247f22e08
Bashar deeb <bashardeeb77@gmail.com>
2024-05-19 17:53:22 +0200

    Another solution : RequestRateLimiter refactored (reduced Overhead with Optimized Solution)

--- src/main/java/com/isw/assessment/client/util/RequestRateLimiter.java
+++ src/main/java/com/isw/assessment/client/util/RequestRateLimiter.java
@@ -1,34 +1,36 @@
 package com.isw.assessment.client.util;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class RequestRateLimiter {
 
-    private final long timeIntervalInNanos;
-    private volatile AtomicLong lastRequestTime = new AtomicLong(0);
-    private volatile Lock lock = new ReentrantLock(true);
+    private final long timeIntervalInMilis;
+    private final AtomicReference<LocalTime> previousRequestTime = new AtomicReference<>(LocalTime.now());
 
     public RequestRateLimiter(long timeIntervalInMilliSeconds) {
-        this.timeIntervalInNanos = TimeUnit.MILLISECONDS.toNanos(timeIntervalInMilliSeconds);
+        this.timeIntervalInMilis = timeIntervalInMilliSeconds;
     }
 
-    public void waitBetweenRequests(){
+    public void waitBetweenRequests() {
 
         try {
-            lock.lock();
-            long duration = System.nanoTime() - lastRequestTime.get();
-            long remainingTimeInNanoSeconds = duration > timeIntervalInNanos ? 0 : timeIntervalInNanos - duration;
+            LocalTime oldRequestTime = previousRequestTime.getAndUpdate(this::getNewRequestTime);
+            long millis = Duration.between(LocalTime.now(), oldRequestTime).toMillis();
 
-            Thread.sleep(TimeUnit.NANOSECONDS.toMillis(remainingTimeInNanoSeconds));
-        } catch (InterruptedException ignored) {
+            Thread.sleep(millis > 0 ? millis : 0);
+        } catch (InterruptedException ignored) {}
 
-        }
-        finally {
-            lastRequestTime.set(System.nanoTime());
-            lock.unlock();
-         };
     }
+
+    private LocalTime getNewRequestTime(LocalTime localTime) {
+        return localTime.isBefore(LocalTime.now()) ?
+                LocalTime.now().plus(timeIntervalInMilis, ChronoUnit.MILLIS) :
+                localTime.plus(timeIntervalInMilis,ChronoUnit.MILLIS);
+    }
+
 }
